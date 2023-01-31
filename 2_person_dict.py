229f8ac74896e318ddc7c1ab54befc2be345b08d
Dianne Reyes <dianne_reyes@baylor.edu>
2023-01-31 13:46:43 -0600

    23Jan31 - Class ex. 2, 3, 4

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,27 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+# print out the name of the second child
+print(person["children"][1])
+# mylist = person['children']
+
+# print out the name of the cat
+print(person["pets"]["cat"])
+
+# use a loop to print out the names of each child
+
+for child in person["children"]:
+    print(child)
+
+# use a loop to print out the pets in the following format:
+# the type of pet is: dog and the name of the pet is: Fido
+
+# pets_dict = person["pets"]
+# for key, value in pets_dict.items():
+# print(key, value)
+
+for key, value in person["pets"].items():
+    print(f"the type of pet is: {key} and the name of the pet is {value}")
